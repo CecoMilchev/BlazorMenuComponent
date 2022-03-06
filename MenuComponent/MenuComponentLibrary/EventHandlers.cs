37e0de97e866c920551b1a96dc60d890c60e5d70
Tsvetomir Milchev <tmilchev@progress.com>
2022-03-06 17:08:10 +0200

    feat: implement expand/collapse for submenus

---
+++ MenuComponent/MenuComponentLibrary/EventHandlers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuComponentLibrary
+{
+    [EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+    [EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+    public static class EventHandlers
+    {
+    }
+}
