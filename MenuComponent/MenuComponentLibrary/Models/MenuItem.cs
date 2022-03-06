883bcebc61181a8fa293bdf695ddc9f28418e085
Tsvetomir Milchev <tmilchev@progress.com>
2022-03-06 15:51:06 +0200

    chore: add initial menu rendering

---
+++ MenuComponent/MenuComponentLibrary/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuComponentLibrary.Models
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Icon { get; set; }
+        public bool IsExpanded { get; set; }
+        public IEnumerable<MenuItem>? MenuItems { get; set; }
+    }
+}
